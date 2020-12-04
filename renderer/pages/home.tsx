36d885914ba2dd6489919e60ad703d2cc1e0c581
Cilvia333 <cilvia333x@gmail.com>
2020-12-04 19:35:48 +0900

    EditorWrapperを追加

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
+import styled, { css } from 'styled-components';
+import tw from 'twin.macro';
 
 import Editor from '~/components/editor/editor';
 
@@ -10,11 +12,15 @@ const Home: React.FC = () => {
       <Head>
         <title>Home - Nextron (with-typescript)</title>
       </Head>
-      <div>
+      <EditorWrapper>
         <Editor />
-      </div>
+      </EditorWrapper>
     </>
   );
 };
+
+const EditorWrapper = styled.div`
+  width: 50%;
+`;
 
 export default Home;
